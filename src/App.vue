5f752c0083c8e0bec7d462f2ee0cadd441e2b712
CNine <cnine@CNjie.cai@thoughtworks.com>
2019-05-20 19:03:44 +0800

    update lint

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     HelloWorld
   }
-};
+}
 </script>
+
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,14 +88,13 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
-};
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
 h3
   margin 40px 0 0
